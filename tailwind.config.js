f104f661e27a32923b752bb72860d17dfce6b02c
LuisSantillan96 <luistecisc@gmail.com>
2023-01-25 18:24:39 -0600

    Adding images

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./public/index.html", "./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      primary: '#2563eb',
+      secundary: '#fef08a'
+    },
     extend: {},
   },
   plugins: [
